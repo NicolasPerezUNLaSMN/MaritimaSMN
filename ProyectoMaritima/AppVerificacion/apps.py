0dbdb7d12db9fefec4e5d484a7ee7b9947297cb1
Santi <59890737+Helvetion2@users.noreply.github.com>
2024-07-22 08:45:27 -0300

    FixCambioNombre

--- ProyectoMaritima/AppVerificacion/apps.py
+++ ProyectoMaritima/AppVerificacion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VerificacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'verificacion'
+    name = 'AppVerificacion'
