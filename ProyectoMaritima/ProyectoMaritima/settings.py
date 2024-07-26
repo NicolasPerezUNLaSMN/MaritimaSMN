a841a9426bf31894977a253ea16cbec1790349b4
Santi <59890737+Helvetion2@users.noreply.github.com>
2024-07-26 18:30:40 -0300

    settngsStatics
    Declaro STATIC_URL = '/static/'
    STATIC_ROOT = os.path.join(BASE_DIR,'AppVerificacion/static/AppVerificacion/')
    
    En settings.py dentro de ProyectoMaritmia/ProyectoMaritima

--- ProyectoMaritima/ProyectoMaritima/settings.py
+++ ProyectoMaritima/ProyectoMaritima/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 
 
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'AppVerificacion/static/AppVerificacion/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
